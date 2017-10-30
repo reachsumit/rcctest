af8d80db888dc6fc08ec1ee2278f3d0eb6500e2c
sumit-code <sam.sumitkumar@gmail.com>
2017-10-30 11:05:20 -0500

    day 1 updates

--- rcctest/rccproj/webapp/models.py
+++ rcctest/rccproj/webapp/models.py
@@ -1,22 +1,14 @@
 from django.db import models
 
-BIKE_TYPE_CHOICES = (
-    ('0', 'Road Bike'),
-    ('1', 'Mountain Bike'),
-    ('2', 'Hybrid Bike'),
-    ('3', 'City Bike'),
-    ('4', 'Others'),
-)
-
 # Create your models here.
 class Bikes(models.Model):
     SKU = models.CharField(max_length=17,null=False,primary_key=True,unique=True)
     name = models.CharField(max_length=50,null=False)
     description = models.CharField(max_length=180,default="No description available.")
-    rating = models.IntegerField(default=0)
+    rating = models.DecimalField(max_digits=1,decimal_places=1,default=0.0)
     price = models.DecimalField(max_digits=8,decimal_places=2,null=False)
     quantity = models.IntegerField(default=0)
-    type = models.CharField(max_length=15, choices=BIKE_TYPE_CHOICES)
+    type = models.CharField(max_length=25,default='others')
     image = models.ImageField()
 
     def __str__(self):
